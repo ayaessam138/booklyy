0f283abcc0eccab9d1a8acc720a9aae421a52c63
aya essam <ayaessam>
2023-08-03 17:54:32 +0300

    bestseller listview done & make bookdetailsview

---
+++ lib/features/home/persentation/views/book_details_view.dart
@@ -0,0 +1,9 @@
+import 'package:booklyy/features/home/persentation/views/weidgets/BookDetails_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: BookDetailsBody());
+  }
+}
