be1a8c6b1e691a1d64758dd95defe613e12ff3c1
aya essam <ayaessam>
2023-08-08 02:44:00 +0300

    newestbook&featuredbook cubit

--- lib/features/home/persentation/view_models/cubit/featured_books_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:booklyy/features/home/data/models/bookmodel/bookmodel.dart';
-import 'package:equatable/equatable.dart';
-
-part 'featured_books_state.dart';
-
-class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-}
